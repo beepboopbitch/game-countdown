60b3e50e93da7f4e49e2b42c1d526fdc5a1d76dd
Student <example@example.com>
2019-02-27 13:57:33 -0800

    add blog and styling

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './sass/styles.scss';
 
 
 
-
+//buiness logic
 
 function get_elapsed_time_string(total_seconds) {
   function pretty_time_string(num) {
@@ -34,8 +34,44 @@ function get_elapsed_time_string(total_seconds) {
 }
 
 
+//ui logic
+
 var elapsed_seconds = 864000;
 setInterval(function() {
   elapsed_seconds = elapsed_seconds - 1;
   $('#box_header').text(get_elapsed_time_string(elapsed_seconds));
 }, 1000);
+
+$(document).ready(function() {
+  event.preventDefault();
+  $(".blog-content-1").hide();
+  $(".hide-1").hide();
+  $(".blog-post-main-1").click(function(){
+    $(".blog-content-1").show();
+    $(".hide-1").show();
+  });
+  $(".hide-1").click(function(){
+    $(".blog-content-1").hide();
+    $(".hide-1").hide();
+  }); //end blog 1
+  $(".blog-content-2").hide();
+  $(".hide-2").hide();
+  $(".blog-post-main-2").click(function(){
+    $(".blog-content-2").show();
+    $(".hide-2").show();
+  });
+  $(".hide-2").click(function(){
+    $(".blog-content-2").hide();
+    $(".hide-2").hide();
+  }); //end blog 2
+  $(".blog-content-3").hide();
+  $(".hide-3").hide();
+  $(".blog-post-main-3").click(function(){
+    $(".blog-content-3").show();
+    $(".hide-3").show();
+  });
+  $(".hide-3").click(function(){
+    $(".blog-content-3").hide();
+    $(".hide-3").hide();
+  }); //end blog 3
+});
